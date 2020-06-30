3169b057419f3b47f20e9d218e04aaef22347a32
Legion <Legion@192.168.1.61>
2020-06-30 16:54:57 -0500

    Mi primer commit

---
+++ src/main/java/com/redsocial/ExamenPc03Application.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenPc03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenPc03Application.class, args);
+	}
+
+}
